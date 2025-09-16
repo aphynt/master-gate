41e076df2c8da8cf7f32ef313cafee71b3397396
ahmadfadillllah <ahmadfadillah502@gmail.com>
2025-09-17 07:21:25 +0800

    update

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<Throwable>>
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     */
+    public function register(): void
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     */
+    public function render($request, Throwable $exception)
+    {
+        // Cek apakah exception ini HttpException
+        if ($exception instanceof HttpExceptionInterface) {
+            $status = $exception->getStatusCode();
+
+            if ($status == 419 || $status == 409) {
+                return redirect()->route('login')
+                    ->with('info', 'Sesi sudah habis, silakan login ulang.');
+            }
+        }
+
+        return parent::render($request, $exception);
+    }
+}
